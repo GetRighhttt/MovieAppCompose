2ebe0bb6da520e95356eaa262a8a27842fdaafc4
stefan bayne <stefanbayne@gmail.com>
2024-02-01 14:12:36 -0500

    making each row clickable.

--- app/src/main/java/com/example/composemovieapp/MainActivity.kt
+++ app/src/main/java/com/example/composemovieapp/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.composemovieapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -14,9 +30,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composemovieapp.ui.theme.ComposeMovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +43,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-
+                MainContent()
             }
         }
     }
@@ -36,6 +55,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     ComposeMovieAppTheme {
+        /**
+         * Scaffold - part of material design that implements the basic material design layout
+         * structure.
+         */
         Scaffold(
             topBar = {
                 TopAppBar(
@@ -50,7 +73,8 @@ fun MyApp(content: @Composable () -> Unit) {
                         )
                     }
                 )
-            }) {
+            },
+        ) {
             content()
         }
     }
@@ -58,16 +82,68 @@ fun MyApp(content: @Composable () -> Unit) {
 
 //holds main content
 @Composable
-fun MainContent() {
-    Surface(
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Comedy",
+        "Fantasy",
+        "Crime",
+        "Drama",
+        "Music",
+        "Adventure",
+        "History",
+        "Thriller",
+        "Animation",
+        "Family",
+        "Comedy",
+        "Fantasy",
+        "Crime",
+        "Drama",
+        "Music",
+        "Adventure",
+        "History",
+        "Thriller",
+        "Animation",
+        "Family"
+    )
+) {
+    Column(modifier = Modifier.padding(top = 70.dp)) {
+        LazyColumn() {
+            items(items = movieList) {
+                MovieRow(movie = it) { movie ->
+                    Log.d("MOVIE", "Current Movie: $movie")
+                }
+            }
+        }
+    }
+}
+
+// passing a click listener to make each row clickable
+@Composable
+fun MovieRow(movie: String, onItemClick: (String) -> Unit = {}) {
+    Card(
         modifier = Modifier
+            .padding(6.dp)
             .fillMaxWidth()
-            .fillMaxHeight()
+            .height(120.dp)
+            .clickable { onItemClick(movie) },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
     ) {
-        /**
-         * Scaffold - part of material design that implements the basic material design layout
-         * structure.
-         */
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(95.dp),
+                shape = RectangleShape,
+                shadowElevation = 5.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Person")
+            }
+            Text(text = movie)
+        }
     }
 }
 
